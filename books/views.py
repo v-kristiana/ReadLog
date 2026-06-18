0e98ce5ced29407395d68c37ddecdbc5a59d0d88
Kristiana <kv25042@edu.lu.lv>
2026-06-18 12:04:01 +0300

    Added status and favourite function

--- books/views.py
+++ books/views.py
@@ -2,6 +2,37 @@ from django.shortcuts import render
 from .models import Book
 from django.shortcuts import render, get_object_or_404
 from django.db.models import Q
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import redirect
+from .models import ReadingStatus
+
+
+@login_required
+def toggle_favourite(request, book_id):
+    if request.method == 'POST':
+        book = get_object_or_404(Book, id=book_id)
+        if request.user.favourites.filter(book=book).exists():
+            request.user.favourites.filter(book=book).delete()
+        else:
+            request.user.favourites.create(book=book)
+    return redirect('book_detail', book_id=book_id)
+
+
+@login_required
+def set_reading_status(request, book_id):
+    book = get_object_or_404(Book, id=book_id)
+    status = request.POST.get('status')
+    if status:
+        try:
+            reading_status = request.user.reading_statuses.get(book=book)
+            reading_status.status = status
+            reading_status.save()
+        except ReadingStatus.DoesNotExist:
+            ReadingStatus.objects.create(user=request.user, book=book, status=status)
+    return redirect('book_detail', book_id=book_id)
+
+
+
 
 def book_detail(request, book_id):
     book = get_object_or_404(Book, id=book_id)
